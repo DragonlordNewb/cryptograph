947f0b4879ab9c0622709832a33c0257ee0eb92a
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-10 08:50:07 -0500

    bleh

--- cryptograph/ui.py
+++ cryptograph/ui.py
@@ -5,11 +5,86 @@ from tkinter import Entry
 from tkinter import Button
 from tkinter import Frame
 
+from abc import ABC 
+from abc import abstractstaticmethod
+
+from typing import Any
+
 from cryptograph import algorithms
 
 class Cryptograph(Tk):
 
 	FONT = lambda size: ("OCR A Extended", size)
+
+	class OperationWindow(Tk):
+		def __init__(self, operation: str, inclKeying: bool=True, **kwargs: dict[str, Any]) -> None:
+			for key in kwargs.keys():
+				setattr(self, key, kwargs[key])
+
+			Tk.__init__(self)
+			self.title("Cryptograph: " + operation)
+			self.config(bg="black")
+
+			self.targetFile = None
+			self.keyFile = None
+
+			self.titleLabel = Label(
+				self,
+				text=operation,
+				bg="black",
+				fg="white",
+				font=Cryptograph.FONT(16)
+			)
+			self.titleLabel.grid(row=1, column=1, columnspan=2)
+
+			if inclKeying:
+				self.keyFileLabel = Label(
+					self,
+					text="Key file",
+					bg="black",
+					fg="white",
+					font=Cryptograph.FONT(12)
+				)
+				self.keyFileLabel.grid(row=2, column=1)
+				self.targetFileLabel = Label(
+					self,
+					text="Target file",
+					bg="black",
+					fg="white",
+					font=Cryptograph.FONT(12)
+				)
+				
+			self.targetFileLabel.grid(row=2, column=2)
+			self.keyFileSelect = Button(
+				self,
+				text="None selected",
+				bg="black",
+				fg="white",
+				font=Cryptograph.FONT(12),
+				command=self.findKeyFile
+			)
+			self.keyFileSelect.grid(row=3, column=1)
+			self.targetFileSelect = Button(
+				self,
+				text="None selected",
+				bg="black",
+				fg="white",
+				font=Cryptograph.FONT(12),
+				command=self.findTargetFile
+			)
+			self.targetFileSelect.grid(row=3, column=2)
+			self.runButton = Button(
+				self,
+				text="Go",
+				bg="black",
+				fg="white",
+				font=Cryptograph.FONT(12),
+				command=lambda: self.run(self.targetFile, self.keyFile)
+			)
+
+		@abstractstaticmethod
+		def run(targetFile: str, keyFile: str) -> None:
+			pass
 
 	def __init__(self) -> None:
 		Tk.__init__(self)
@@ -75,3 +150,8 @@ class Cryptograph(Tk):
 			command=lambda: self.decryptFile()
 		)
 		self.decryptFileButton.grid(row=3, column=1)
+
+		self.algorithmFrame = Frame(self, bg="black")
+		self.algorithmFrame.grid(row=3, column=1, columspan=2)
+
+		self.
